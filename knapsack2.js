e164fdb42d21388d3da36e38fe0cd5f5deffa77a
Isaac Yoon <ysaacioon@yahoo.com>
2017-08-04 22:52:54 -0700

    knapsack

---
+++ knapsack2.js
@@ -0,0 +1,19 @@
+function cakeTheif(items, capacity){
+    let maxValue = 0;
+    function knapsackHelper(item, capacity, currVal) {
+        //when the capacity reaches 0, then compare it with the maxValue and reset the maxValue
+        if(capacity === 0 && currVal > maxValue) {
+            maxValue = currVal;
+        }
+        //if the weight exceeds capacity, then return
+        if(capacity < 0) {
+            return;
+        }
+        //create all possible combinations here.
+        for(var i = 0; i < items.length; i++) {
+            knapsackHelper(items, capacity-items[i][0], currVal + items[i][1])
+        }
+    }
+    knapsackHelper(items, capacity, 0)
+    return maxValue;
+}
